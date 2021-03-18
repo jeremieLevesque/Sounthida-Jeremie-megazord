a9eaf6ac771e50d16e381366bde02e8a9fca492b
Jérémie Lévesque <70386087+jeremieLevesque@users.noreply.github.com>
2021-03-17 21:22:37 -0400

    jeremie

--- Scripts/discographie.js
+++ Scripts/discographie.js
@@ -1,14 +1,21 @@
-const swiper = new Swiper('.swiper-container-discographie', {
 
-  direction: 'horizontal',
-  loop: false,
-  speed: 800,
+
+var swiper = new Swiper('.swiper-container-discographie', {
+  direction: 'vertical',
+  effect: 'flip',
   slidesPerView: 1,
-  effect: 'slide',
-
-
+  spaceBetween: 30,
+  centeredSlides: true,
+  autoplay: {
+    delay: 2500,
+    disableOnInteraction: false,
+  },
   pagination: {
     el: '.swiper-pagination-discographie',
+    clickable: true,
   },
-
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
 });
