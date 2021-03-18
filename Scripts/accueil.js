ccd9514cedd6d6cfc93e4a93b32be14e7beba385
Sounthi <sounthikg@gmail.com>
2021-03-17 20:18:02 -0400

    push

--- Scripts/accueil.js
+++ Scripts/accueil.js
@@ -1,1 +1,15 @@
+const swiper = new Swiper('.swiper-container-accueil', {
 
+    direction: 'horizontal',
+    loop: false,
+    speed: 800,
+    slidesPerView: 1,
+    effect: 'slide',
+
+
+    pagination: {
+      el: '.swiper-pagination-accueil',
+    },
+
+  });
+  
